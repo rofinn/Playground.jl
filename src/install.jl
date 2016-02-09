cb66f8746b09da04eec99efaee0de99647094419
Curtis Vogt <curtis.vogt@gmail.com>
2016-02-09 09:15:27 -0600

    Fix octal integer mistake with chmod

--- src/install.jl
+++ src/install.jl
@@ -129,7 +129,7 @@ end
                 end
                 if app_path != nothing
                     copy(app_path, src_path)
-                    chmod(exe_path, 00755)
+                    chmod(exe_path, 0o755)
                 end
             finally
                 run(`hdiutil detach $mountdir`)
@@ -171,7 +171,7 @@ end
             readdir(src_path)[1],
             "bin/julia"
         )
-        chmod(julia_bin_path, 00755)
+        chmod(julia_bin_path, 0o755)
         mklink(julia_bin_path, bin_path)
 
         return bin_path
