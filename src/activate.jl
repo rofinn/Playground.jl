818300195b010b4622fdab98b9d910eb70218d40
Curtis Vogt <curtis.vogt@gmail.com>
2016-02-16 17:19:07 -0600

    Refactor OS specific macros
    Note that the *_only functions were unindented by 4 spaces. No other
    changes were made to those functions.

--- src/activate.jl
+++ src/activate.jl
@@ -20,47 +20,43 @@ function activate(config::Config; dir::AbstractString="", name::AbstractString="
 end
 
 
-@windows_only begin
-    function run_shell(config, prompt)
-        run(`cmd /K prompt $(prompt)`)
-    end
+@windows_only function run_shell(config, prompt)
+    run(`cmd /K prompt $(prompt)`)
 end
 
 
-@unix_only begin
-    function run_shell(config, prompt)
-        ENV["PS1"] = prompt
-        if haskey(ENV, "SHELL") && contains(ENV["SHELL"], "fish")
-            run(`$(ENV["SHELL"]) -i`)
-        elseif haskey(ENV, "SHELL")
-            # Try and setup the new shell as close to the user's default shell as possible.
-            usr_rc = joinpath(homedir(), "." * basename(ENV["SHELL"]) * "rc")
-            pg_rc = joinpath(dirname(ENV["JULIA_PKGDIR"]), basename(ENV["SHELL"]) * "rc")
+@unix_only function run_shell(config, prompt)
+    ENV["PS1"] = prompt
+    if haskey(ENV, "SHELL") && contains(ENV["SHELL"], "fish")
+        run(`$(ENV["SHELL"]) -i`)
+    elseif haskey(ENV, "SHELL")
+        # Try and setup the new shell as close to the user's default shell as possible.
+        usr_rc = joinpath(homedir(), "." * basename(ENV["SHELL"]) * "rc")
+        pg_rc = joinpath(dirname(ENV["JULIA_PKGDIR"]), basename(ENV["SHELL"]) * "rc")
 
-            if !ispath(pg_rc)
-                cp(usr_rc, pg_rc)
-                fstream = open(pg_rc, "a")
-                try
-                    path = ENV["PATH"]
-                    ps1 = ENV["PS1"]
-                    pkg_dir = ENV["JULIA_PKGDIR"]
+        if !ispath(pg_rc)
+            cp(usr_rc, pg_rc)
+            fstream = open(pg_rc, "a")
+            try
+                path = ENV["PATH"]
+                ps1 = ENV["PS1"]
+                pkg_dir = ENV["JULIA_PKGDIR"]
 
-                    write(fstream, "export PATH=$path\n")
-                    write(fstream, "export PS1=\"$(prompt)\"\n")
-                    write(fstream, "export JULIA_PKGDIR=$pkg_dir\n")
+                write(fstream, "export PATH=$path\n")
+                write(fstream, "export PS1=\"$(prompt)\"\n")
+                write(fstream, "export JULIA_PKGDIR=$pkg_dir\n")
 
-                    if haskey(ENV, "HISTFILE")
-                        histfile = ENV["HISTFILE"]
-                        write(fstream, "export HISTFILE=$histfile\n")
-                    end
-                finally
-                    close(fstream)
+                if haskey(ENV, "HISTFILE")
+                    histfile = ENV["HISTFILE"]
+                    write(fstream, "export HISTFILE=$histfile\n")
                 end
+            finally
+                close(fstream)
             end
-
-            run(`$(ENV["SHELL"]) --rcfile $pg_rc`)
-        else
-            run(`sh -i`)
         end
+
+        run(`$(ENV["SHELL"]) --rcfile $pg_rc`)
+    else
+        run(`sh -i`)
     end
 end
